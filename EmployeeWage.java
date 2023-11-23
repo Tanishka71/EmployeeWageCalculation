37016f85937557be509d53ad7aea794dc5fa4e1c
Tanishka Jain <tanishkajain1508@gmail.com>
2023-11-23 18:46:48 +0530

    EmployeeWage.java

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,11 +2,27 @@ package EmployeeWage;
 
 public class EmployeeWage {
 	/*
-	 * @params:main
+	 * @params:hoursWorked
 	 * @return:none
-	 * @desc:prints msg
+	 * @desc:checks whether the employee is full time or part time
 	 */	 
+    private static void checkAttendance() {
+        double attendanceValue = Math.random();
+
+        if (attendanceValue > 0.5) {
+            System.out.println("Employee is present.");
+        } else {
+            System.out.println("Employee is absent.");
+        }
+    }
+    
+ 	/*
+ 	 * @params:none
+ 	 * @return:none
+ 	 * @desc:prints whether employee is present or absent
+ 	 */	 
     public static void main(String[] args){
-        System.out.println("Welcome to EMPLOYEE WAGE COMPUTATION PROGRAM!!");
+        System.out.println("ATTENDANCE of the employee:");
+        checkAttendance();
     }
 }
