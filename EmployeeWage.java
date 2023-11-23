e64956b0416150996683535410030f1019a1a29a
Tanishka Jain <tanishkajain1508@gmail.com>
2023-11-23 19:04:46 +0530

    UC_6_total_wage_calculation_till_conditions_are_true

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -8,30 +8,64 @@ public class EmployeeWage {
 	
 	
 	/*
-	 * @params:daysWorked
-	 * @return:integer(dailyWage of the employee)
-	 * @desc:calculates the monthly wage of the employee
-	 */	
-    public static int monthlyAmount(int daysWorked){
-        int monthlyWage = WAGE_PER_HOUR * daysWorked;
-        return monthlyWage;
+	 * @params:hoursWorked
+	 * @return:integer(1/0)
+	 * @desc:checks whether the employee is full time or part time
+	 */	  
+    private static int checkPartFullTime(int hoursWorked) {
+        if(hoursWorked == full_day && hoursWorked>0) {
+        	return 1;
+        }
+        else {
+        	return 0; 
+    }  
     }
     
     
 	/*
+	 * @params:hoursWorked
+	 * @return:integer(dailyWage of the employee)
+	 * @desc:calculates the daily wage of the employee
+	 */	
+    static int calculateWage(int hoursWorked) {
+        int dailyWage = hoursWorked * WAGE_PER_HOUR;
+        return dailyWage;
+    }
+     
+    
+	/*
 	 * @params:none
 	 * @return:none
-	 * @desc:calls the function monthly wage and prints the monthly income
-	 */	 
+	 * @desc:calls the function calculateWage,checkPartFullTime and prints the daywise income & total income
+	 */
     public static void main(String[] args){
         System.out.println("Welcome to Employee Wage Computation Program");
         Random random=new Random();
         int daysWorked=random.nextInt(21);
-        int monthlyWage = monthlyAmount(daysWorked);
-    	System.out.println("The number of days this employee worked:"+ daysWorked);
-        System.out.print("The monthly wage of full time employee: "+ monthlyWage);
+        int totalIncome=0,totalHours=0,i=1;
+        while(i<=daysWorked && totalHours<=100) {
+                int hoursWorked=random.nextInt(8)+1;
 
-
+                    int check=checkPartFullTime(hoursWorked);
+                    if(check ==1) {
+                    	System.out.println(" Day "+i+" full time:160$");
+                        totalIncome +=(full_day * WAGE_PER_HOUR);	
+                        totalHours +=full_day;
+                        i++;
+                    }
+                    else {
+                        int dailyWage=calculateWage(hoursWorked);
+                        totalIncome +=dailyWage;
+                        totalHours +=hoursWorked;
+                 
+                        System.out.println(" Day "+i+" part time:"+dailyWage+"$");
+                         i++;
+                    }
+                 }
+              	
+        System.out.println("total salary:"+totalIncome+"$");
+        System.out.println("total hours:"+totalHours+"hours");
     }
+
 	
 }
