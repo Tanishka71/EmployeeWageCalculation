ae8dabaa5609557e8d8ccb0561bd4ef51fafe632
Tanishka Jain <tanishkajain1508@gmail.com>
2023-11-23 18:59:41 +0530

    UC_5_monthly_wage_calculation

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,64 +6,32 @@ public class EmployeeWage {
 	static final int WAGE_PER_HOUR=20;
 	static final int full_day=8;
 	
+	
 	/*
-	 * @params:hoursWorked
-	 * @return:integer(1/0)
-	 * @desc:checks whether the employee is full time or part time
-	 */
-    private static int checkPartFullTime(int hoursWorked) {
-        if(hoursWorked == full_day && hoursWorked>0) {
-        	return 1;
-        }
-        else if(hoursWorked < full_day && hoursWorked>0){
-        	return 0; 
-    }
-        else {
-        	return -1;
-        } 
+	 * @params:daysWorked
+	 * @return:integer(dailyWage of the employee)
+	 * @desc:calculates the monthly wage of the employee
+	 */	
+    public static int monthlyAmount(int daysWorked){
+        int monthlyWage = WAGE_PER_HOUR * daysWorked;
+        return monthlyWage;
     }
     
     
 	/*
-	 * @params:hoursWorked
-	 * @return:returns daily wage
-	 * @desc:calls the function checkPartFulltime and calculates the daily wage using switch case
-	 */
-	   private static int calculateDailyWage(int hoursWorked) {
-	        
-	        int check=checkPartFullTime(hoursWorked);
-	        int dailyWage;
-	        switch (check) {
-	            case 1:
-	            	System.out.println("Full time employee.");
-	                dailyWage=WAGE_PER_HOUR * hoursWorked;
-	                break;
-	            case 0:
-	            	System.out.println("Part time employee."); 
-	            	 dailyWage=WAGE_PER_HOUR * hoursWorked;
-	                break;
-	            case -1:
-	                System.out.println("Employee is absent.");
-	                dailyWage= 0;
-	                break;
-	             default:
-	            	 System.out.println("Invalid input.");
-	            	 return 0;
-	        }
-	        return dailyWage;
-	    }
-	   
-		/*
-		 * @params:none
-		 * @return:none
-		 * @desc:prints the wage of the employee
-		 */
-	    public static void main(String[] args){
-	        System.out.println("Welcome to Employee Wage Computation Program");
-	        Random random=new Random();
-	        int hoursWorked=random.nextInt(9);
-	        int dailyWage=calculateDailyWage(hoursWorked);
-	        System.out.println("The wage of the employee is:$"+dailyWage);
+	 * @params:none
+	 * @return:none
+	 * @desc:calls the function monthly wage and prints the monthly income
+	 */	 
+    public static void main(String[] args){
+        System.out.println("Welcome to Employee Wage Computation Program");
+        Random random=new Random();
+        int daysWorked=random.nextInt(21);
+        int monthlyWage = monthlyAmount(daysWorked);
+    	System.out.println("The number of days this employee worked:"+ daysWorked);
+        System.out.print("The monthly wage of full time employee: "+ monthlyWage);
 
-	    }
+
+    }
+	
 }
