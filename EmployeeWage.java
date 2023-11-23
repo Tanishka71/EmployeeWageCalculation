63b975b53f106800c52c7f43b4db4f52fbc2e85f
Tanishka Jain <tanishkajain1508@gmail.com>
2023-11-23 18:43:19 +0530

    Print message

---
+++ EmployeeWage.java
@@ -0,0 +1,12 @@
+package EmployeeWage;
+
+public class EmployeeWage {
+	/*
+	 * @params:main
+	 * @return:none
+	 * @desc:prints msg
+	 */	 
+    public static void main(String[] args){
+        System.out.println("Welcome to EMPLOYEE WAGE COMPUTATION PROGRAM!!");
+    }
+}
