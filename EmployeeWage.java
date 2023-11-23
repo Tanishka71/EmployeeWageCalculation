b5bda447f19bba0ac356da14256ec05776a8d9f7
Tanishka Jain <tanishkajain1508@gmail.com>
2023-11-23 18:57:01 +0530

    UC_4_switch_case_wage_calculation

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -8,34 +8,62 @@ public class EmployeeWage {
 	
 	/*
 	 * @params:hoursWorked
-	 * @return:integer(dailyWage of the employee)
-	 * @desc:calculates the daily wage of the employee
-	 */	  
-     static int calculateWage(int hoursWorked) {
-        int dailyWage = hoursWorked * WAGE_PER_HOUR;
-        return dailyWage;
-    }
-     
-     
-		/*
-		 * @params:main
-		 * @return:none
-		 * @desc:creates object for random class and prints wage calling calculateWage function
-		 */	 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
-        Random random=new Random();
-        int hoursWorked=random.nextInt(9);
+	 * @return:integer(1/0)
+	 * @desc:checks whether the employee is full time or part time
+	 */
+    private static int checkPartFullTime(int hoursWorked) {
         if(hoursWorked == full_day && hoursWorked>0) {
-        	System.out.println("Full time employee.");
+        	return 1;
         }
-        else if(hoursWorked<8 && hoursWorked>0){
-        	System.out.println("Part time employee."); 
+        else if(hoursWorked < full_day && hoursWorked>0){
+        	return 0; 
     }
         else {
-        	System.out.println("Employee is absent");
-        }
-        int dailyWage = calculateWage(hoursWorked);
-        System.out.println("Daily Wage of this employee: $" + dailyWage);
-}
+        	return -1;
+        } 
+    }
+    
+    
+	/*
+	 * @params:hoursWorked
+	 * @return:returns daily wage
+	 * @desc:calls the function checkPartFulltime and calculates the daily wage using switch case
+	 */
+	   private static int calculateDailyWage(int hoursWorked) {
+	        
+	        int check=checkPartFullTime(hoursWorked);
+	        int dailyWage;
+	        switch (check) {
+	            case 1:
+	            	System.out.println("Full time employee.");
+	                dailyWage=WAGE_PER_HOUR * hoursWorked;
+	                break;
+	            case 0:
+	            	System.out.println("Part time employee."); 
+	            	 dailyWage=WAGE_PER_HOUR * hoursWorked;
+	                break;
+	            case -1:
+	                System.out.println("Employee is absent.");
+	                dailyWage= 0;
+	                break;
+	             default:
+	            	 System.out.println("Invalid input.");
+	            	 return 0;
+	        }
+	        return dailyWage;
+	    }
+	   
+		/*
+		 * @params:none
+		 * @return:none
+		 * @desc:prints the wage of the employee
+		 */
+	    public static void main(String[] args){
+	        System.out.println("Welcome to Employee Wage Computation Program");
+	        Random random=new Random();
+	        int hoursWorked=random.nextInt(9);
+	        int dailyWage=calculateDailyWage(hoursWorked);
+	        System.out.println("The wage of the employee is:$"+dailyWage);
+
+	    }
 }
