055a3ad44b8f5404e6ce476d6b93a1625d2c8036
Tanishka71 <86073425+Tanishka71@users.noreply.github.com>
2023-11-26 17:32:14 +0530

    Update EmployeeWage.java

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,105 +1,145 @@
-package EmployeeWage;
 
+package com.bridgelazbz.employee;
+
+import java.util.*;
+
+
+import java.util.Scanner;
 import java.util.Random;
 
-public class EmployeeWage {
-	static final int WAGE_PER_HOUR=20;
-	static final int full_day=8;
-	
-	
-	/*
-	 * @params:none
-	 * @return:integer(1/0)
-	 * @desc:checks whether the employee is present or absent
-	 */	  
-	static int checkAttendance() {
-		double attendanceValue = Math.random();
-		if (attendanceValue > 0.5) {
-			return 1;
-		} else {
-			return 0;
-		}
-	}
-	
-	
-	/*
-	 * @params:hoursWorked
-	 * @return:integer(1/0)
-	 * @desc:checks whether the employee is full time or part time
-	 */	  
-    private static int checkPartFullTime(int hoursWorked) {
-        if(hoursWorked == full_day && hoursWorked>0) {
-        	return 1;
-        }
-        else {
-        	return 0; 
-    }    
-    }
-    
-    
-	/*
-	 * @params:hoursWorked
-	 * @return:integer(dailyWage of the employee)
-	 * @desc:calculates the daily wage of the employee
-	 */	  
-    static int calculateWage(int hoursWorked) {
-        int dailyWage = hoursWorked * WAGE_PER_HOUR;
-        return dailyWage;
-    }
-    
-   
-     
-	/*
-	 * @params:daysWorked
-	 * @return:none
-	 * @desc:calls the function checkAttendance ,calculateWage,checkPartFullTime and prints the daywise income & total income
-	 */	 
-    static void monthlyAmount(int daysWorked){
-    	Random random=new Random();
-        int totalIncome=0,totalHours=0,i=1;
-        while(i<=daysWorked && totalHours<=100) {
-        	    int presence=checkAttendance();
-        	    if(presence ==1) {
-                    int hoursWorked=random.nextInt(8)+1;
+/*@desc: Class for calculation of employee wage */
+class EmpWageBuilder {
 
-                    int check=checkPartFullTime(hoursWorked);
-                    if(check ==1) {
-                    	System.out.println(" Day "+i+" full time:160$");
-                        totalIncome +=(full_day * WAGE_PER_HOUR);	
-                        totalHours +=full_day;
-                        i++;
-                    }
-                    else {
-                        int dailyWage=calculateWage(hoursWorked);
-                        totalIncome +=dailyWage;
-                        totalHours +=hoursWorked;
-                 
-                        System.out.println(" Day "+i+" part time:"+dailyWage+"$");
-                         i++;
-                    }
-        	    }
-        	    else {
-        	    	System.out.println(" Day "+i+" employee is absent:");
-        	    	i++;
-        	    }
-            	
+    // Instance variables for each company
+    private final String companyName;
+    private final int wagePerHour;
+    private final int fullDayHours;
+
+    // Instance variables to store total wage and total hours
+    private int totalWage;
+    private int totalHours;
+
+    /*
+     * @desc: Constructor to initialize company-specific details
+     * @params: companyName - Name of the company
+     *          wagePerHour - Wage per hour for the company
+     *          fullDayHours - Full working hours for the company
+     */
+    public EmpWageBuilder(String companyName, int wagePerHour, int fullDayHours) {
+        this.companyName = companyName;
+        this.wagePerHour = wagePerHour;
+        this.fullDayHours = fullDayHours;
+    }
+
+    /*
+     * @desc: Simulates monthly wage calculation
+     * @params: daysWorked - Number of days worked for the company
+     * @return: None
+     */
+    public void calculateMonthlyWage(int daysWorked) {
+        Random random = new Random();
+        int i = 1;
+        while (i <= daysWorked && totalHours <= 100) {
+            int presence = checkAttendance();
+            if (presence == 1) {
+                int hoursWorked = random.nextInt(fullDayHours) + 1;
+                int check = checkPartFullTime(hoursWorked);
+                if (check == 1) {
+                    System.out.println(" Day " + i + " for " + companyName + " full time: " + fullDayHours * wagePerHour + "$");
+                    totalWage += (fullDayHours * wagePerHour);
+                    totalHours += fullDayHours;
+                } else {
+                    int dailyWage = calculateWage(hoursWorked);
+                    totalWage += dailyWage;
+                    totalHours += hoursWorked;
+
+                    System.out.println(" Day " + i + " for " + companyName + " part time: " + dailyWage + "$");
                 }
-        System.out.println(" total salary:"+totalIncome+"$");
-        System.out.println(" total hours:"+totalHours+"hours");
+                i++;
+            } else {
+                System.out.println(" Day " + i + " for " + companyName + " employee is absent");
+                i++;
+            }
+        }
+        // Print total salary and total hours worked for the company
+        System.out.println(" Total salary for " + companyName + ": " + totalWage + "$");
+        System.out.println(" Total hours worked for " + companyName + ": " + totalHours + " hours");
     }
 
-	/*
-	 * @params:main
-	 * @return:none
-	 * @desc:creates object for random class and takes days randomly
-	 */	 
-    public static void main(String[] args){
+    /*
+     * @desc: Checks whether the employee is present or absent
+     * @return: 1 for present, 0 for absent
+     */
+    private int checkAttendance() {
+        double attendanceValue = Math.random();
+        return attendanceValue > 0.5 ? 1 : 0;
+    }
+
+    /*
+     * @desc: Checks whether the employee is full time or part time
+     * @params: hoursWorked - Number of hours worked by the employee
+     * @return: 1 for full time, 0 for part time
+     */
+    private int checkPartFullTime(int hoursWorked) {
+        return hoursWorked == fullDayHours && hoursWorked > 0 ? 1 : 0;
+    }
+
+    /*
+     * @desc: Calculates the daily wage based on the hours worked
+     * @params: hoursWorked - Number of hours worked by the employee
+     * @return: Daily wage of the employee
+     */
+    private int calculateWage(int hoursWorked) {
+        return hoursWorked * wagePerHour;
+    }
+}
+
+public class EmployeeWage {
+
+    /*
+     * @desc: Main method to execute the employee wage computation program
+     * @params: args - command-line arguments (not used in this program)
+     * @return: None
+     */
+    public static void main(String[] args) {
         System.out.println("Welcome to EMPLOYEE WAGE COMPUTATION PROGRAM!!");
         System.out.println();
-        Random random=new Random();
-        int daysWorked=random.nextInt(21);
-        monthlyAmount(daysWorked);
 
+        // Create Scanner object to take user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Get details for Company1 from the user
+        System.out.println("Enter details for Company1:");
+        System.out.print("Company Name: ");
+        String company1Name = scanner.next();
+        System.out.print("Wage Per Hour: ");
+        int company1WagePerHour = scanner.nextInt();
+        System.out.print("Full Day Hours: ");
+        int company1FullDayHours = scanner.nextInt();
+
+        // Get details for Company2 from the user
+        System.out.println("Enter details for Company2:");
+        System.out.print("Company Name: ");
+        String company2Name = scanner.next();
+        System.out.print("Wage Per Hour: ");
+        int company2WagePerHour = scanner.nextInt();
+        System.out.print("Full Day Hours: ");
+        int company2FullDayHours = scanner.nextInt();
+
+        // Create instances of EmpWageBuilder for each company
+        EmpWageBuilder company1 = new EmpWageBuilder(company1Name, company1WagePerHour, company1FullDayHours);
+        EmpWageBuilder company2 = new EmpWageBuilder(company2Name, company2WagePerHour, company2FullDayHours);
+
+        // Get the number of days worked for each company from the user
+        System.out.print("Enter number of days worked for " + company1Name + ": ");
+        int company1DaysWorked = scanner.nextInt();
+        company1.calculateMonthlyWage(company1DaysWorked);
+
+        System.out.print("Enter number of days worked for " + company2Name + ": ");
+        int company2DaysWorked = scanner.nextInt();
+        company2.calculateMonthlyWage(company2DaysWorked);
+
+        // Close the scanner
+        scanner.close();
     }
-	
 }
